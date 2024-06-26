554e6322ddd13f5b27e38a2bc6e31343fcb9de33
dhelms33 <derrickhelms10@gmail.com>
2024-06-26 14:54:50 -0600

    Added cpp files

---
+++ firstprogram.cpp
@@ -0,0 +1,6 @@
+//#include <iostream> //preprocessor directive
+
+int main() {
+    std::cout << "Hello World!" << std::endl; //output line
+    return 0; //return statement
+}
---
+++ secondprogram.cpp
@@ -0,0 +1,11 @@
+
+float subtract() {
+    float a = 10.01;
+    float b = 4.31;
+    return a - b;
+}
+float main() {
+    int differene = subtract();
+    std:: "The difference is " << difference << std::endl;
+    return 0;
+}
