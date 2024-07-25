adf6b264d64ee96c2119f32d80bddaa86a355504
dhelms33 <derrickhelms10@gmail.com>
2024-07-25 13:19:15 -0600

    Added switch statements

---
+++ if_else.cpp
@@ -0,0 +1,12 @@
+int main() {
+    int x = 50;
+    int y = 100;
+    if (x > y) {
+        cout >> "X is THE CHAMPION!! X is greater than Y!" << endL; 
+}
+else {
+    cout >> "Y is THE ULTIMATE VICTOR!! Y is greater than X!" << endL;
+}
+    return 0;
+}
+
---
+++ switch.cpp
@@ -0,0 +1,11 @@
+int main () {
+    int day = 3;
+    switch(day) {
+        case 1:
+            cout << "Tuesday";
+            break;
+        case 2:
+            cout << "Wednesday";
+            break;
+    }
+}
