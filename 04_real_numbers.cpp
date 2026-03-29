2f6d4efacd72373864b8033712141fcab15fd419
dhelms33 <derrickhelms10@gmail.com>
2026-03-29 17:30:51 -0600

    Added first cpp double program

--- 04_real_numbers.cpp
+++ 04_real_numbers.cpp
@@ -3,7 +3,11 @@
 int main() {
     float a = 34.5f;
     float b = 22.76f;
+    double c = 45.676767;
     float answer = a + b;
-    std::cout << "The answer is " << answer; 
+    std::cout << "The answer is " << answer << std::endl; 
+    // output the values
+    std::cout << "The float is " << a << std::endl;
+    std::cout << "The double is " << c << std::endl;
     return 0;
 }
