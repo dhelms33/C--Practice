054051e69ee710167badaf499ae3e8777264d686
dhelms33 <derrickhelms10@gmail.com>
2026-03-31 16:30:43 -0600

    Added coddy_bool.cpp

---
+++ 06_coddy_string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    string coddy = "I am learning to write c++ on Coddy"; // initialize string
+
+    std::cout << "This is my first string: " << coddy << std::endl;// initialize cout
+    return 0;
+
+}
---
+++ 07_coddy_bool.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main {
+    bool is_true = true;
+
+    std::cout << "This is my first time initializing a bool, it prints 1 " << is_true <<std::endl;
+    return 0;
+}
