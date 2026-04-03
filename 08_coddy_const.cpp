8f38476394fe6f4cfb18ca7acb816dc29d563e33
dhelms33 <derrickhelms10@gmail.com>
2026-04-02 18:12:32 -0600

    Added const

---
+++ 08_coddy_const.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    const int MAX_VALUE = 456;
+    std::cout << "This is a constant, it cannot be changed" << MAX_VALUE << endl;
+    return 0;
+}
