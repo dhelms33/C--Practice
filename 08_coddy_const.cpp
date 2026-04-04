015c1154d02485b32abd718fc86c99301956abf8
dhelms33 <derrickhelms10@gmail.com>
2026-04-03 22:27:13 -0600

    Added PI

--- 08_coddy_const.cpp
+++ 08_coddy_const.cpp
@@ -3,6 +3,8 @@
 
 int main() {
     const int MAX_VALUE = 456;
+    const double PI = 3.14;
     std::cout << "This is a constant, it cannot be changed" << MAX_VALUE << endl;
+    std::cout << "This is a constant for PI, " << PI << endl;
     return 0;
 }
