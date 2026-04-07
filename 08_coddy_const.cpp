e600046569ed90ea71d3d30dcac5ee165db26288
dhelms33 <derrickhelms10@gmail.com>
2026-04-06 23:24:42 -0600

    Added var for studentNumber

--- 08_coddy_const.cpp
+++ 08_coddy_const.cpp
@@ -4,7 +4,9 @@
 int main() {
     const int MAX_VALUE = 456;
     const double PI = 3.14;
+    const double studentNumber;
     std::cout << "This is a constant, it cannot be changed" << MAX_VALUE << endl;
     std::cout << "This is a constant for PI, " << PI << endl;
+    std::cout << "This is a variable for studentNumber " << studentNumber << endl;
     return 0;
 }
