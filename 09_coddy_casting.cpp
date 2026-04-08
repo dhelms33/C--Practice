5d149475bdb0d76ea297d96840c8ddc16c837458
dhelms33 <derrickhelms10@gmail.com>
2026-04-08 17:06:58 -0600

    Added main()

--- 09_coddy_casting.cpp
+++ 09_coddy_casting.cpp
@@ -6,5 +6,8 @@ int main() {
     //this is an implicit cast 
     int number = 5;
     double decimal = number;
+
+    double weight = 54.6;
+    int roundedWeight = (int) weight; //becomes 54
     return 0;
 }
