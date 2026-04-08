a3f546842b3a19ce7a567a8d514e2246ebec5b6a
dhelms33 <derrickhelms10@gmail.com>
2026-04-07 19:31:24 -0600

    Added name in main

--- 08_coddy_const.cpp
+++ 08_coddy_const.cpp
@@ -5,8 +5,10 @@ int main() {
     const int MAX_VALUE = 456;
     const double PI = 3.14;
     const double studentNumber;
+    std::string name = "Dereck";
     std::cout << "This is a constant, it cannot be changed" << MAX_VALUE << endl;
     std::cout << "This is a constant for PI, " << PI << endl;
     std::cout << "This is a variable for studentNumber " << studentNumber << endl;
+    std::cout << "This is a variable for name " << name << endl;
     return 0;
 }
