25b7443b2c8716f310cd38500b38e4fe02f96738
dhelms33 <derrickhelms10@gmail.com>
2026-04-08 17:02:58 -0600

    Added new main()

---
+++ 09_coddy_casting.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name = "Dereck";
+    //this is an implicit cast 
+    int number = 5;
+    double decimal = number;
+    return 0;
+}
