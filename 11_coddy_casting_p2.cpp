a1d8141859078d96541970db8135a10ecd1eed89
dhelms33 <derrickhelms10@gmail.com>
2026-04-12 00:00:36 -0600

    added coddy casting

--- 11_coddy_casting_p2.cpp
+++ 11_coddy_casting_p2.cpp
@@ -3,6 +3,11 @@
 
 int main(){
     double num = 54.0;
+    int num2 = 54;
+    std::string name = "Dereck";
     string testNum = to_string(num);
+    int testInt = stoi(num2);
+    //The stoi() function converts a string representation of a number into an integer value.
+    double testDouble = stod(test);
     return 0;
 }
